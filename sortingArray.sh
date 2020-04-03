c4194497c9ca59aad284921a869468dc53c62b03
Tushar-W <twankhede95@gmail.com>
2020-04-03 19:36:33 +0530

    REFACTORED: added fuction for store in dictionary and array

--- sortingArray.sh
+++ sortingArray.sh
@@ -5,69 +5,76 @@ declare -A computeResults
 declare -a resultsInArray
 
 function addAndMultiplyNum() {
-a=$1 b=$2 c=$3
-echo $(($a + $b * $c))
+ a=$1 b=$2 c=$3
+ echo $(($a + $b * $c))
 }
 
 function multiplyAndAddNum() {
-a=$1 b=$2 c=$3
-echo $(($a * $b + $c))
+ a=$1 b=$2 c=$3
+ echo $(($a * $b + $c))
 }
 
 function addAndDivideNum() {
-a=$1 b=$2 c=$3
-echo $(($c + $a / $b))
+ a=$1 b=$2 c=$3
+ echo $(($c + $a / $b))
 }
 
 function modAndAddNum() {
-a=$1 b=$2 c=$3
-echo $(($a % $b + $c))
+	a=$1 b=$2 c=$3
+	echo $(($a % $b + $c))
 }
 
 function sortDescendingOrder() {
 
-for (( count=0; count<4; ++count ))
-do
-for (( j=$((count+1)); j<4; ++j))
-do
-if [ ${resultsInArray[count]} -lt ${resultsInArray[j]} ];
-then
-temp=${resultsInArray[count]}
-resultsInArray[count]=${resultsInArray[j]}
-resultsInArray[j]=$temp
-fi
-done
-done
-
+ for (( count=0; count<${#resultsInArray[@]}; ++count ))
+ do
+  for (( j=$((count+1)); j<${#resultsInArray[@]}; ++j))
+  do
+   if [ ${resultsInArray[count]} -lt ${resultsInArray[j]} ];
+   then
+    temp=${resultsInArray[count]}
+    resultsInArray[count]=${resultsInArray[j]}
+	 resultsInArray[j]=$temp
+	fi
+  done
+ done
 }
 
 function sortAscendingOrder() {
 
-for (( count=0; count<4; ++count ))
-do
-for (( j=$((count+1)); j<4; ++j))
-do
-if [ ${resultsInArray[count]} -gt ${resultsInArray[j]} ];
-then
-temp=${resultsInArray[count]}
-resultsInArray[count]=${resultsInArray[j]}
-resultsInArray[j]=$temp
-fi
-done
-done
+ for (( count=0; count<${#resultsInArray[@]}; ++count ))
+ do
+  for (( j=$((count+1)); j<${#resultsInArray[@]}; ++j))
+  do
+   if [ ${resultsInArray[count]} -gt ${resultsInArray[j]} ];
+	then
+	 temp=${resultsInArray[count]}
+	 resultsInArray[count]=${resultsInArray[j]}
+	 resultsInArray[j]=$temp
+	fi
+  done
+ done
+}
+
+function storeInDictionary() {
+ computeResults["addAndMultiply"]=$( addAndMultiplyNum $a $b $c )
+ computeResults["multiplyAndAdd"]=$( multiplyAndAddNum $a $b $c )
+ computeResults["addAndDivide"]=$( addAndDivideNum $a $b $c )
+ computeResults["modAndAdd"]=$( modAndAddNum $a $b $c )
+}
+
+function storeInArray() {
+ resultsInArray[0]=${computeResults["addAndMultiply"]}
+ resultsInArray[1]=${computeResults["multiplyAndAdd"]}
+ resultsInArray[2]=${computeResults["addAndDivide"]}
+ resultsInArray[3]=${computeResults["modAndAdd"]}
 
 }
 
 read -p "Enters Three Numbers:" a b c
-computeResults["addAndMultiply"]=$( addAndMultiplyNum $a $b $c )
-computeResults["multiplyAndAdd"]=$( multiplyAndAddNum $a $b $c )
-computeResults["addAndDivide"]=$( addAndDivideNum $a $b $c )
-computeResults["modAndAdd"]=$( modAndAddNum $a $b $c )
+storeInDictionary
 #reading values from dictionary into Array
-resultsInArray[0]=${computeResults["addAndMultiply"]}
-resultsInArray[1]=${computeResults["multiplyAndAdd"]}
-resultsInArray[2]=${computeResults["addAndDivide"]}
-resultsInArray[3]=${computeResults["modAndAdd"]}
+storeInArray
 # sorting values of array in Descending order
 sortDescendingOrder
 echo ${resultsInArray[@]}
