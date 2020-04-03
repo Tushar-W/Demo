22164623dc6663ade80f3a12044163436f222f97
Tushar-W <twankhede95@gmail.com>
2020-04-03 18:59:41 +0530

    MODIFIED: space indentation

--- sortingArray.sh
+++ sortingArray.sh
@@ -5,49 +5,59 @@ declare -A computeResults
 declare -a resultsInArray
 
 function addAndMultiplyNum() {
-	a=$1 b=$2 c=$3
-	echo $(($a + $b * $c))
+a=$1 b=$2 c=$3
+echo $(($a + $b * $c))
 }
+
 function multiplyAndAddNum() {
-	a=$1 b=$2 c=$3
-	echo $(($a * $b + $c))
+a=$1 b=$2 c=$3
+echo $(($a * $b + $c))
 }
+
 function addAndDivideNum() {
-	a=$1 b=$2 c=$3
-	echo $(($c + $a / $b))
+a=$1 b=$2 c=$3
+echo $(($c + $a / $b))
 }
+
 function modAndAddNum() {
-	a=$1 b=$2 c=$3
-	echo $(($a % $b + $c))
+a=$1 b=$2 c=$3
+echo $(($a % $b + $c))
 }
+
 function sortDescendingOrder() {
-	for (( count=0; count<4; ++count ))
-	do
-		for (( j=$((count+1)); j<4; ++j))
-		do
-			if [ ${resultsInArray[count]} -lt ${resultsInArray[j]} ];
-			then
-				temp=${resultsInArray[count]}
-				resultsInArray[count]=${resultsInArray[j]}
-				resultsInArray[j]=$temp
-			fi
-		done
-	done
+
+for (( count=0; count<4; ++count ))
+do
+for (( j=$((count+1)); j<4; ++j))
+do
+if [ ${resultsInArray[count]} -lt ${resultsInArray[j]} ];
+then
+temp=${resultsInArray[count]}
+resultsInArray[count]=${resultsInArray[j]}
+resultsInArray[j]=$temp
+fi
+done
+done
+
 }
+
 function sortAscendingOrder() {
-	for (( count=0; count<4; ++count ))
-	do
-	 for (( j=$((count+1)); j<4; ++j))
-	 do
-	  if [ ${resultsInArray[count]} -gt ${resultsInArray[j]} ];
-	  then
-	   temp=${resultsInArray[count]}
-		resultsInArray[count]=${resultsInArray[j]}
-		resultsInArray[j]=$temp
-	  fi
-	 done
-	done
+
+for (( count=0; count<4; ++count ))
+do
+for (( j=$((count+1)); j<4; ++j))
+do
+if [ ${resultsInArray[count]} -gt ${resultsInArray[j]} ];
+then
+temp=${resultsInArray[count]}
+resultsInArray[count]=${resultsInArray[j]}
+resultsInArray[j]=$temp
+fi
+done
+done
+
 }
+
 read -p "Enters Three Numbers:" a b c
 computeResults["addAndMultiply"]=$( addAndMultiplyNum $a $b $c )
 computeResults["multiplyAndAdd"]=$( multiplyAndAddNum $a $b $c )
